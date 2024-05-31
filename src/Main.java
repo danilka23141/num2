6d180101b835af7d6500fe758759ccaa6b528cc3
Mikhalev Daniil <mixalyov1994@list.ru>
2024-05-31 19:57:55 +0300

    Редакт1

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,33 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        // Начальная сумма на счету
+        int initialBalance = 0;
+
+        // Сумма пополнения (вводится с клавиатуры)
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите сумму пополнения: ");
+        int topUpAmount = scanner.nextInt();
+
+        // Порог для начисления бонуса
+        int bonusThreshold = 1000;
+
+        // Количество бонусных рублей
+        int bonus = 0;
+
+        // Проверяем, превышает ли сумма пополнения порог
+        if (topUpAmount > bonusThreshold) {
+            // Рассчитываем количество бонусных рублей
+            bonus = topUpAmount / 100;
         }
+
+        // Рассчитываем итоговую сумму на счету
+        int finalBalance = initialBalance + topUpAmount + bonus;
+
+        // Выводим результаты
+        System.out.println("Итоговая сумма на счету: " + finalBalance + " рублей");
+        System.out.println("Количество бонусных рублей: " + bonus);
     }
 }
